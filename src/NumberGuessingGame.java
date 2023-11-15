4cfd1289b7b13a8224b06f3dc1b2627e07a6162e
alexjames4 <alexanderjamesmansfield@gmail.com>
2023-11-15 17:20:51 +0000

    random number generated and is viewable in cmd

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,2 +1,9 @@
 public class NumberGuessingGame {
+    public static void main(String[] args) {
+
+        //generate a random number between 1-50
+        int randomNumber = (int)Math.round(Math.random()*50);
+
+        System.out.println(randomNumber);
+    }
 }
